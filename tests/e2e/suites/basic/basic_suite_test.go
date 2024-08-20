ccf47ea1f5ae0ccfd1aa6c69174d528fd05b1d24
Franco <franco@giantswarm.io>
2024-08-20 11:38:14 -0300

    Add kyverno tests

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -1,18 +1,19 @@
 package basic
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/giantswarm/apptest-framework/pkg/config"
 	"github.com/giantswarm/apptest-framework/pkg/state"
 	"github.com/giantswarm/apptest-framework/pkg/suite"
-	"github.com/giantswarm/clustertest/pkg/organization"
-	"github.com/giantswarm/clustertest/pkg/wait"
+	"github.com/giantswarm/clustertest/pkg/logger"
+
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 const (
@@ -26,19 +27,33 @@ func TestBasic(t *testing.T) {
 		interval         = time.Millisecond * 250
 		appReadyTimeout  = 10 * time.Minute
 		appReadyInterval = 5 * time.Second
-		kyvernoNamespace = "kyverno"
 		bundleNamespace  = "security-bundle"
+		installNamespace = "kyverno"
 	)
 
 	suite.New(config.MustLoad("../../config.yaml")).
+		InAppBundle("security-bundle").
 		// The namespace to install the app into within the workload cluster
-		WithInstallNamespace("kube-system").
+		WithInstallNamespace(installNamespace).
 		// If this is an upgrade test or not.
 		// If true, the suite will first install the latest released version of the app before upgrading to the test version
 		WithIsUpgrade(isUpgrade).
 		WithValuesFile("./values.yaml").
 		AfterClusterReady(func() {
-			// Do any pre-install checks here (ensure the cluster has needed pre-reqs)
+
+			It("should connect to the management cluster", func() {
+				err := state.GetFramework().MC().CheckConnection()
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should connect to the workload cluster", func() {
+				wcClient, err := state.GetFramework().WC(state.GetCluster().Name)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = wcClient.CheckConnection()
+				Expect(err).NotTo(HaveOccurred())
+			})
+
 		}).
 		BeforeUpgrade(func() {
 			// Perform any checks between installing the latest released version
@@ -46,31 +61,30 @@ func TestBasic(t *testing.T) {
 			// E.g. ensure that the initial install has completed and has settled before upgrading
 		}).
 		Tests(func() {
-			var org *organization.Org
 
-			BeforeSuite(func() {
-				org = state.GetCluster().Organization
+			It("has the app running in the cluster", func() {
+				wcClient, err := state.GetFramework().WC(state.GetCluster().Name)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(func() error {
+					logger.Log("Checking if deployment exists in the workload cluster")
+					var dp appsv1.Deployment
+					err := wcClient.Get(state.GetContext(), types.NamespacedName{Namespace: installNamespace, Name: "kyverno-admission-controller"}, &dp)
+					if err != nil {
+						logger.Log("Failed to get deployment: %v", err)
+					}
+					return err
+				}).
+					WithPolling(5 * time.Second).
+					WithTimeout(5 * time.Minute).
+					ShouldNot(HaveOccurred())
 			})
 
-			Describe("Check Apps status", func() {
-
-				It("should have kyverno, kyverno-policies and kyverno-policy-operator deplyoed", func() {
-					Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-kyverno", state.GetCluster().Name), org.GetNamespace())).
-						WithTimeout(appReadyTimeout).
-						WithPolling(appReadyInterval).
-						Should(BeTrue())
-
-					Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-kyverno-policies", state.GetCluster().Name), org.GetNamespace())).
-						WithTimeout(appReadyTimeout).
-						WithPolling(appReadyInterval).
-						Should(BeTrue())
-
-					Eventually(wait.IsAppDeployed(state.GetContext(), state.GetFramework().MC(), fmt.Sprintf("%s-kyverno-policy-operator", state.GetCluster().Name), org.GetNamespace())).
-						WithTimeout(appReadyTimeout).
-						WithPolling(appReadyInterval).
-						Should(BeTrue())
-				})
-			})
 		}).
-		Run(t, "Basic Test")
+		AfterSuite(func() {
+
+			logger.Log("Cleaning up after tests have completed")
+
+		}).
+		Run(t, "Default Bundle App Test")
 }
