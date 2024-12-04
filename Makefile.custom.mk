79c2a2b0e750ed6ec6c148465422e4dc0f636906
Franco <franco@giantswarm.io>
2024-12-04 16:02:32 -0300

    Install CRDs before tests

---
+++ Makefile.custom.mk
@@ -0,0 +1,8 @@
+kind-create:
+	kind create cluster --name kyverno --image=kindest/node:${KUBERNETES_VERSION}
+
+kind-get-kubeconfig:
+	kind get kubeconfig --name kyverno > kyverno-kubeconfig.yaml
+
+install-kyverno:
+	helm install https://giantswarm.github.io/giantswarm-catalog/kyverno-crds-${KYVERNO_CRDS_VERSION}.tgz
