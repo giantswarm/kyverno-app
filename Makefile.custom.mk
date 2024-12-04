a2163c49f307272a2133fc94fd883820a47afa9a
Franco <franco@giantswarm.io>
2024-12-04 16:06:59 -0300

    Generate name for kyverno-crds release

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,4 +5,4 @@ kind-get-kubeconfig:
 	kind get kubeconfig --name kyverno > kyverno-kubeconfig.yaml
 
 install-kyverno:
-	helm install https://giantswarm.github.io/giantswarm-catalog/kyverno-crds-${KYVERNO_CRDS_VERSION}.tgz
+	helm install https://giantswarm.github.io/giantswarm-catalog/kyverno-crds-${KYVERNO_CRDS_VERSION}.tgz --generate-name
