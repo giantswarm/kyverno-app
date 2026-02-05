20f2c48260bcac0777924eb36d3bc4786b5db7dd
Spyros Synodinos <spyros@giantswarm.io>
2026-02-05 17:24:43 +0200

    curl fix

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,7 +5,7 @@ kind-get-kubeconfig:
 	kind get kubeconfig --name kyverno > kyverno-kubeconfig.yaml
 
 install-vpa-crds:
-	kubectl --kubeconfig=kyverno-kubeconfig.yaml apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/vertical-pod-autoscaler-1.3.0/vertical-pod-autoscaler/deploy/vpa-v1-crd-gen.yaml
+	curl -sL https://raw.githubusercontent.com/kubernetes/autoscaler/vertical-pod-autoscaler-1.3.0/vertical-pod-autoscaler/deploy/vpa-v1-crd-gen.yaml | kubectl --kubeconfig=kyverno-kubeconfig.yaml apply -f -
 
 install-kyverno:
 	helm install https://giantswarm.github.io/giantswarm-catalog/kyverno-crds-${KYVERNO_CRDS_VERSION}.tgz --generate-name --kubeconfig=kyverno-kubeconfig.yaml
